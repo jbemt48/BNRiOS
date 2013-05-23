c727648472dd7e224a8b2d1e2639d51a963e3bf0
Joseph Berman <JB@Moshes-MacBook.local>
2013-05-22 22:28:49 -0400

    Initial Commit

---
+++ RandomPossessions/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomPossessions
+//
+//  Created by Joseph Berman on 5/22/13.
+//  Copyright (c) 2013 Joseph Berman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
